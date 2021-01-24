32f9cfa9b29824725b4b56db62f8d269d0b96b79
AaronAnima <1700012896@pku.edu.cn>
2021-01-25 01:49:01 +0800

    init

---
+++ rllab/mujoco_py/__init__.py
@@ -0,0 +1,10 @@
+from .mjviewer import MjViewer
+from .mjcore import MjModel
+from .mjcore import register_license
+import os
+from .mjconstants import *
+
+# register_license(os.path.join(os.path.dirname(__file__),
+#                               '~/.mujoco/mjkey.txt'))
+
+register_license('/home/wmingd/.mujoco/mjkey.txt')
